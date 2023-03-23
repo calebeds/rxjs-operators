1044ff914c53bf1d49282245f1e0e44d5657a303
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-23 10:21:46 -0300

    add syncronous example

--- src/async.js
+++ src/async.js
@@ -1,1 +1,22 @@
-import { add } from './helpers';
+import { add } from "./helpers";
+
+add.li("Line 3");
+
+const callback = (message) => {
+  add.li(message);
+};
+
+const greeting = (message, cb) => {
+  let start = Date.now();
+  for (let i = 0; i < 1000000000; i++) {
+    //do nothing
+  }
+  add.li(`took: ${Date.now() - start}ms`);
+  cb(message);
+};
+
+greeting("Hello from line 18", callback);
+
+add.li("Line 20");
+
+add.li("Line 22");
