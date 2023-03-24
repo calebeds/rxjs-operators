16f2b2a4534675bb9bf5f53673d044ebf7869ba4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-24 09:46:27 -0300

    create an observable and unsubscribe to it

--- src/observable.js
+++ src/observable.js
@@ -1,1 +1,32 @@
-import { add } from "./helpers"
+import { add } from "./helpers";
+import { Observable } from "rxjs";
+
+const o = new Observable((observer) => {
+  setInterval(() => {
+    observer.next("Observable every second");
+  }, 1000);
+});
+
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise!");
+  }, 1000);
+});
+
+// Promise
+p.then((message) => {
+  add.li(message);
+});
+
+//Observable
+const subscription = o.subscribe({
+  next: (message) => {
+    add.li(message);
+  },
+  error: (error) => console.error(error),
+  complete: () => add.li("This Observable is complete"),
+});
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 2000);
