9c6c9f084441899a84cede46764895d463041941
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-26 16:45:51 -0300

    add tap example

--- src/tap.js
+++ src/tap.js
@@ -1,3 +1,16 @@
-import {add} from "./helpers";
-import {} from "rxjs/operators";
+import { add } from "./helpers";
+import { interval } from "rxjs";
+import { tap, map, take } from "rxjs/operators";
 
+const counter = interval(1000);
+
+counter
+  .pipe(
+    take(10),
+    tap((value) => add.li(`before x2: ${value}`)),
+    map((x) => Math.pow(x, 2)),
+    tap((value) => add.li(`after x2: ${value}`)),
+    map((x) => Math.sqrt(x)),
+    tap((value) => add.li(`after square root: ${value}`))
+  )
+  .subscribe(add.li);
