901001ed39ed398783936574bf9b1cb4bd925567
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-26 16:31:00 -0300

    add map-filter example

--- src/map-filter.js
+++ src/map-filter.js
@@ -1,25 +1,17 @@
-import {add} from "./helpers"
-import {interval} from "rxjs";
-import {map, filter, take} from "rxjs/operators";
+import { add } from "./helpers";
+import { interval } from "rxjs";
+import { map, filter, take } from "rxjs/operators";
 
+const numbers = ["zero", "one", "two", "three", "four"];
+const counter$ = interval(1000).pipe(take(4));
 
-const numbers = ['zero', 'one', 'two', 'three', 'four'];
-const counter = interval(1000).pipe( take(4) );
-
-counter.pipe(
-  filter(
-      (value) => {
-          return value % 2 === 0
-      }
-  ),
-  map(
-      (value)=>{
-          return numbers[value]
-      }
+counter$
+  .pipe(
+    filter((value) => {
+      return value % 2 === 0;
+    }),
+    map((value) => {
+      return numbers[value];
+    })
   )
-).subscribe(
-    add.li
-)
-
-
-
+  .subscribe(add.li);
