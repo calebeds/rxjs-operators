76d0af3819311bb522f0f681c5df39fc36364815
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-26 18:16:55 -0300

    add skip example

--- src/skip.js
+++ src/skip.js
@@ -1,5 +1,17 @@
-import {add} from './helpers'
-import {from, fromEvent} from 'rxjs';
-import {take, skip, skipWhile, skipLast} from 'rxjs/operators';
+import { add } from "./helpers";
+import { from, fromEvent, interval } from "rxjs";
+import { take, skip, skipWhile, skipLast, skipUntil } from "rxjs/operators";
 
+from(["apples", "grapes", "oranges", "pears"]).pipe(skipLast(2));
+//   .subscribe(add.li);
 
+const button = document.getElementById("submit");
+const buttonEvents = fromEvent(button, "click");
+
+interval(1000)
+  .pipe(
+    // take(10),
+    skipUntil(buttonEvents)
+    // skipWhile((number) => number < 4)
+  )
+  .subscribe(add.li);
