c3522e4ca8d41570cd2e0dfd893052b01087d78b
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-26 20:18:50 -0300

    add throttle example

--- src/throttle.js
+++ src/throttle.js
@@ -1,4 +1,10 @@
-import {add} from "./helpers";
-import {interval, animationFrameScheduler} from "rxjs";
-import {throttle, take} from "rxjs/operators";
+import { add } from "./helpers";
+import { interval, animationFrameScheduler } from "rxjs";
+import { throttle, take } from "rxjs/operators";
 
+interval(0, animationFrameScheduler)
+  .pipe(
+    throttle(() => interval(1000)),
+    take(10)
+  )
+  .subscribe(add.li);
