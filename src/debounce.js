f419b0f30b5c9c28ba85435e5ae730862e8b1404
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-31 17:41:00 -0300

    add debouce example

--- src/debounce.js
+++ src/debounce.js
@@ -1,4 +1,21 @@
-import {add} from "./helpers";
-import {fromEvent, interval} from "rxjs";
-import {debounce, take, debounceTime} from "rxjs/operators";
+import { add } from "./helpers";
+import { fromEvent, interval } from "rxjs";
+import { debounce, take, debounceTime } from "rxjs/operators";
 
+const inputBox = document.getElementById("input");
+
+const renderBox = document.getElementById("display-content");
+
+const submitBox = document.getElementById("submit");
+
+const content = fromEvent(inputBox, "keyup");
+const submit = fromEvent(submitBox, "click");
+
+content
+  .pipe(
+    // debounce(() => submit)
+    debounceTime(2000)
+  )
+  .subscribe(() => {
+    renderBox.innerHTML = inputBox.value;
+  });
