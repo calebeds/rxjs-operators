2572a1755200b20c2921d4da165b9d88bcf50f5b
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-01 20:57:22 -0300

    add scan example

--- src/scan-reduce.js
+++ src/scan-reduce.js
@@ -1,4 +1,18 @@
-import {add} from "./helpers";
-import {interval, from} from "rxjs";
-import {scan,reduce, take, concatMap} from "rxjs/operators";
+import { add } from "./helpers";
+import { interval, from } from "rxjs";
+import { scan, reduce, take, concatMap } from "rxjs/operators";
 
+const FS = interval(1000).pipe(
+  take(10),
+  scan(
+    (acc, value) => {
+      const n = value + 1;
+      const last = acc[n];
+      const beforeLast = acc[n - 1];
+      return [...acc, last + beforeLast];
+    },
+    [0, 1]
+  )
+);
+
+FS.subscribe(add.li);
