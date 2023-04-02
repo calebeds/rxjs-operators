3a363f162ea9eb7ab08858f21aed08ac64251a08
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-02 06:46:37 -0300

    add concat example

--- src/concat.js
+++ src/concat.js
@@ -1,3 +1,9 @@
-import {add} from './helpers';
-import { concat, interval, fromEvent} from 'rxjs';
-import {take, map} from 'rxjs/operators';
+import { add } from "./helpers";
+import { concat, interval, fromEvent } from "rxjs";
+import { take, map } from "rxjs/operators";
+
+const button = document.getElementById("submit");
+const streamOne = interval(1000).pipe(take(10));
+const streamTwo = fromEvent(button, "click").pipe(map((e) => "clicked"));
+
+concat(streamOne, streamTwo).subscribe(add.li);
