b88468c6723d99af69daa2c707064d9c7b3d5c94
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-02 10:28:10 -0300

    add switch map example

--- src/switch-map.js
+++ src/switch-map.js
@@ -1,18 +1,35 @@
-import { fromEvent } from 'rxjs';
-import { 
-    map, 
-    switchMap, 
-    takeUntil,
-    tap 
-} from 'rxjs/operators';
+import { from, fromEvent } from "rxjs";
+import { map, switchMap, takeUntil, tap } from "rxjs/operators";
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
+const moves$ = fromEvent(canvas, "mousemove");
+const down$ = fromEvent(canvas, "mousedown");
+const up$ = fromEvent(canvas, "mouseup");
 
-function brush (coords){
-    context.lineWidth = 5;
-    context.lineTo(coords.x, coords.y);
-    context.stroke();
-}
+const brush = (coords) => {
+  context.lineWidth = 5;
+  context.lineTo(coords.x, coords.y);
+  context.stroke();
+};
 
+down$
+  .pipe(
+    tap((event) => {
+      context.strokeStyle = "blue";
+      context.beginPath();
+      context.moveTo(event.offsetX, event.offsetY);
+    }),
+    switchMap((event) => {
+      return moves$.pipe(
+        map((event) => {
+          return { x: event.offsetX, y: event.offsetY };
+        }),
+        takeUntil(up$)
+      );
+    })
+  )
+  .subscribe((coords) => {
+    brush(coords);
+  });
