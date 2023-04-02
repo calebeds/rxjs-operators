e59be6b3604589c77e3d3f064f1dadbf77a955d4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-02 08:41:23 -0300

    add concat all example

--- src/concat-all.js
+++ src/concat-all.js
@@ -1,4 +1,16 @@
-import {add} from './helpers';
-import {interval, fromEvent} from 'rxjs'
-import {mergeAll, map, tap, take} from 'rxjs/operators';
+import { add } from "./helpers";
+import { interval, fromEvent } from "rxjs";
+import { concatAll, map, tap, take } from "rxjs/operators";
 
+const button = document.getElementById("submit");
+const clicks = fromEvent(button, "click");
+
+const source = clicks.pipe(
+  tap((ev) => add.li("click")),
+  map(
+    //Inner
+    (ev) => interval(1000).pipe(take(3))
+  )
+);
+
+source.pipe(concatAll()).subscribe(add.li);
