f657e47605bb980cc63654c18f7a5ac1557b1bf4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-02 06:57:49 -0300

    add combine-latest example

--- src/concat.js
+++ src/concat.js
@@ -1,9 +1,13 @@
 import { add } from "./helpers";
-import { concat, interval, fromEvent } from "rxjs";
+import { combineLatest, interval, fromEvent } from "rxjs";
 import { take, map } from "rxjs/operators";
 
 const button = document.getElementById("submit");
-const streamOne = interval(1000).pipe(take(10));
-const streamTwo = fromEvent(button, "click").pipe(map((e) => "clicked"));
+const streamOne$ = interval(1000).pipe(take(10));
+const streamTwo$ = fromEvent(button, "click").pipe(map((e) => "clicked"));
+const streamThree$ = interval(10).pipe(take(50));
+const streamFour$ = interval(2500).pipe(take(3));
 
-concat(streamOne, streamTwo).subscribe(add.li);
+combineLatest(streamOne$, streamTwo$, streamThree$, streamFour$).subscribe(
+  add.li
+);
