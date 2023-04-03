ec4c01c8b520f32b1edd8db9311372aa85de4590
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-03 08:55:50 -0300

    add concatMap example

--- src/merge-map.js
+++ src/merge-map.js
@@ -1,12 +1,12 @@
 import { add, animate } from "./helpers";
 import { fromEvent, interval } from "rxjs";
-import { exhaustMap, switchMap, mergeMap, map, take } from "rxjs/operators";
+import { exhaustMap, switchMap, concatMap, map, take } from "rxjs/operators";
 
 interval(2000)
   .pipe(
     take(3),
     map((value) => `${value * 100}`),
-    mergeMap((x) => {
+    concatMap((x) => {
       return interval(1000).pipe(
         take(3),
         map((value) => `inner(${value}), outer(${x})`)
