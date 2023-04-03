b0c8a376428ca466c3e9746723a865e9cf78413e
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-03 09:06:40 -0300

    add fromFetch example

--- src/http.js
+++ src/http.js
@@ -1,4 +1,13 @@
-import {add} from './helpers';
+import { add } from "./helpers";
+import { from } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+import { switchMap } from "rxjs/operators";
 
+// const getUsers = fetch("https://jsonplaceholder.typicode.com/users");
 
+// const users = from(getUsers);
+const users = fromFetch("https://jsonplaceholder.typicode.com/users");
 
+users.pipe(switchMap((response) => response.json())).subscribe((users) => {
+  users.forEach((user) => add.li(user.name));
+});
