3b347d6a4ad0ccc0055ae51f3655a750dfe83d78
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-03 09:25:00 -0300

    add custom operators example

--- src/http.js
+++ src/http.js
@@ -1,13 +1,24 @@
 import { add } from "./helpers";
 import { from } from "rxjs";
 import { fromFetch } from "rxjs/fetch";
-import { switchMap } from "rxjs/operators";
+import { of, pipe } from "rxjs";
+import { switchMap, mergeMap, concatMap, delay } from "rxjs/operators";
+
+//Custom Operators
+const getJSON = () => {
+  return switchMap((response) => response.json());
+};
+
+const emitEach = (d) => {
+  return pipe(
+    mergeMap((response) => of(...response)),
+    concatMap((response) => of(response).pipe(delay(d)))
+  );
+};
 
 // const getUsers = fetch("https://jsonplaceholder.typicode.com/users");
 
 // const users = from(getUsers);
 const users = fromFetch("https://jsonplaceholder.typicode.com/users");
 
-users.pipe(switchMap((response) => response.json())).subscribe((users) => {
-  users.forEach((user) => add.li(user.name));
-});
+users.pipe(getJSON(), emitEach(2000)).subscribe((user) => add.li(user.name));
