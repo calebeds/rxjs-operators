c129b2ca61944b970273c4cd79aef049eae5bd13
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-04 09:24:12 -0300

    add replay subject example

--- src/replay-subject.js
+++ src/replay-subject.js
@@ -1,30 +1,14 @@
-import {add} from "./helpers";
-import {ReplaySubject, interval} from "rxjs";
-import {take} from "rxjs/operators";
-
+import { add } from "./helpers";
+import { ReplaySubject, interval } from "rxjs";
+import { take } from "rxjs/operators";
 
 const nums = new ReplaySubject(1);
 
-interval(1000).subscribe(
-    (value)=>{
-        nums.next(value)
-    }
-    
-);
+interval(1000).subscribe((value) => {
+  nums.next(value);
+});
+setTimeout(() => {
+  nums.pipe(take(5)).subscribe((value) => add.li(`first: ${value}`));
+}, 5000);
 
-setTimeout(
-    ()=>{
-        nums.pipe(take(5)).subscribe(
-            (value)=>{
-                add.li(`first: ${value}`)
-            }
-        );
-    }, 5000
-)
-
-
-nums.pipe(take(5)).subscribe(
-    (value)=>{
-        add.li(`second: ${value}`)
-    }
-);
+nums.pipe(take(5)).subscribe((value) => add.li(`second: ${value}`));
